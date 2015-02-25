c1176025f24411b505fdc66255d0ac14712d046d
NiteWatch <lighteningtiger68@gmail.com>
2015-02-25 16:55:25 -0600

    Initial work on the GUI

---
+++ GUIWindow.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GUIWindow extends JFrame{
+	
+	//label titles
+	private JLabel pictureLabel;
+	private JLabel imageLabel;
+	private JLabel nameLabel;
+	private JLabel idLabel;
+	
+	//label textfields
+	private JTextField pictureTF;
+	private JTextField imageTF;
+	private JTextField nameTF;
+	private JTextField idTF;
+	
+	public GUIWindow() 
+	{
+		//sets GUI design
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
