671a0f8a496428717505ec927a80ae17d7de4d9b
NiteWatch <lighteningtiger68@gmail.com>
2015-03-27 13:07:39 -0500

    Added necessary .jar files

--- database_console/DBConnect.java
+++ database_console/DBConnect.java
@@ -3,11 +3,12 @@ package database_console;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import com.microsoft.sqlserver.jdbc.SQLServerResultSet;
 
 public class DBConnect {
 
-	static final String JBDC_DRIVER = "com.mysql.jbdc.Driver";
-	static final String DB_URL = "OS-CASE-1.usd233.local";
+	static final String JBDC_DRIVER = "";
+	static final String DB_URL = "jbdc:sqlserver//OS-CASE-1.usd233.local/events";
 	
 	static final String uName = "adrian";
 	static final String uPass = "flores";
@@ -17,7 +18,11 @@ public class DBConnect {
 		
 		try
 		{	
-			Connection con = DriverManager.getConnection(DB_URL, uName, uPass);
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+	         Connection con = DriverManager.getConnection(DB_URL, uName, uPass);
+			//Driver myDriver = new com.microsoft.sqlserver.jdbc.SQLServerDriver();
+			//DriverManager.registerDriver(arg0)
+			
 		}
 		catch(SQLException error)
 		{
