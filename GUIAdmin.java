e723f9320edffa2af697aedb6b7d020a0470ffdf
NiteWatch <lighteningtiger68@gmail.com>
2015-04-05 23:43:55 -0500

    Added components to the Admin Interface

--- GUIAdmin.java
+++ GUIAdmin.java
@@ -1,12 +1,11 @@
 import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
-
+import javax.swing.Box;
 import database_console.DBConnect;
 
-public class GUIAdmin {
+public class GUIAdmin extends JFrame{
 
 	//label titles
 	private JLabel firstNameLabel;
@@ -27,9 +26,15 @@ public class GUIAdmin {
 	private JButton newEventB;
 	private JButton editEventB;
 	
+	//variables to handle events
+	private sortHandler sortHandler;
+	private historyHandler historyHandler;
+	private newEventHandler newEventHandler;
+	private editEventHandler editEventHandler;
+	
 	//set dimensions
-	private static final int WIDTH = 300;
-	private static final int HEIGHT = 300;
+	private static final int WIDTH = 600;
+	private static final int HEIGHT = 400;
 	
 	public GUIAdmin()
 	{
@@ -37,23 +42,77 @@ public class GUIAdmin {
 		setTitle("Administrator Page");
 		
 		//panel for sidebar
-		JPanel c1 = new JPanel();
-		c1.setLayout(new GridLayout(3, 1));
+		JPanel c1 = new JPanel(new FlowLayout());
+		c1.setLayout(new GridLayout(4, 1));
 		c1.add(sortB = new JButton("Sort"));
 		c1.add(historyB = new JButton("History"));
 		c1.add(newEventB = new JButton("New Event"));
-		c1.add(editEventB = new JButton("Edit Event"));
+		
+		JButton editEventB = new JButton("Edit Event");
+		//c1.add(editEventB = new JButton("Edit Event"));
+		editEventB.setLocation(34, 92);
+		c1.add(editEventB);
 		
 		//panel for information labels
 		JPanel c2 = new JPanel();
 		c2.setLayout(new GridLayout(1, 4));
-		c2.add(eventLabel = new JLabel());
-		c2.add(timeLabel = new JLabel());
-		c2.add(dateLabel = new JLabel());
-		c2.add(numberLabel = new JLabel());
+		c2.add(eventLabel = new JLabel("Event"));
+		c2.add(timeLabel = new JLabel("Time"));
+		c2.add(dateLabel = new JLabel("Date"));
+		c2.add(numberLabel = new JLabel("Number Attended"));
 		
 		//panel for information display
 		
+		//add all created panels
+		JPanel c = new JPanel(new FlowLayout());
+		c.add(c1, BorderLayout.SOUTH);
+		c.add(Box.createHorizontalStrut(40));
+		c.add(c2, BorderLayout.NORTH);
+		add(c);
+		
+		//add action listeners
+		sortB.addActionListener(new sortHandler());
+		historyB.addActionListener(new historyHandler());
+		newEventB.addActionListener(new newEventHandler());
+		editEventB.addActionListener(new editEventHandler());
+		
+		//set dimensions
+		setSize(WIDTH, HEIGHT);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationByPlatform(true);
+		setVisible(true);
+	}
+	
+	private class sortHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			 
+		}
+	}
+	
+	private class historyHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			 
+		}
+	}
+	
+	private class newEventHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			 
+		}
+	}
+	
+	private class editEventHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			 
+		}
 	}
 	
 	public static void main(String[] args) {
